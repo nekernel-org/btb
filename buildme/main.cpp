7a76789c12e33e98eff1e5651cb08f50987d96b8
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-06-20 09:14:04 +0200

    Initial Commit

---
+++ buildme/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  buildme
+//
+//  Created by Amlal on 6/20/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
